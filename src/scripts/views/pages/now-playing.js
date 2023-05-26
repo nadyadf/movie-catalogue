68ce12b79f23287a7dfd5fe99be9584851cba206
nadyadf <nadyadf3@gmail.com>
2023-05-26 17:01:05 +0700

    show API content

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -1,16 +1,24 @@
 /* eslint-disable quotes */
 import TheMovieDbSource from "../../data/themoviedb-source";
+import { createMovieItemTemplate } from '../templates/template-creator';
 
 const NowPlaying = {
   async render() {
     return `
-    <h1>Now Playing</h1>
+      <div class="content">
+        <h2 class="content__heading">Now Playing in Cinema</h2>
+        <div id="movies" class="movies">
+        </div>
+      </div>
   `;
   },
 
   async afterRender() {
     const movies = await TheMovieDbSource.nowPlayingMovies();
-    console.log(movies);
+    const moviesContainer = document.querySelector('#movies');
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
--- src/scripts/views/pages/upcoming.js
+++ src/scripts/views/pages/upcoming.js
@@ -1,16 +1,24 @@
 /* eslint-disable quotes */
 import TheMovieDbSource from "../../data/themoviedb-source";
+import { createMovieItemTemplate } from '../templates/template-creator';
 
 const Upcoming = {
   async render() {
     return `
-      <h1>UpComing Page</h1>
+      <div class="content">
+        <h2 class="content__heading">Upcoming in Cinema</h2>
+        <div id="movies" class="movies">
+        </div>
+      </div>
     `;
   },
 
   async afterRender() {
     const movies = await TheMovieDbSource.upcomingMovies();
-    console.log(movies);
+    const moviesContainer = document.querySelector('#movies');
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
