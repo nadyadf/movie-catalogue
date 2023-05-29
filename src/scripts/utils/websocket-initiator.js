21355d1195b61333a850a980e77fe410e567bbf4
nadyadf <nadyadf3@gmail.com>
2023-05-29 17:30:21 +0700

    refactor ke inject manifest dan pembuatan notification

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,12 +1,24 @@
+import CONFIG from '../globals/config';
+import NotificationHelper from './notification-helper';
+
 const WebsocketInitiator = {
+
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMessageHandler;
   },
 
   _onMessageHandler(message) {
-    console.log(message.data);
-    // console.log(JSON.parse(message));
+    const movie = JSON.parse(message.data);
+    console.log(movie);
+
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
 };
 
